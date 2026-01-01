6cc9bd5d93197f41aac126044a100dc7130f3001
inhye94 <inhye.park.dev@gmail.com>
2026-01-01 10:01:55 +0900

    feat: html 파일 만들어서 호출하기(templates)

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from wanted_class import WantedScraper
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("JobScrapper")
 
 @app.route("/")
 def home():
-  return "Hello! Welcome to Job Scrapper!"
+  return render_template("home.html", name="inhye")
+
+@app.route("/hello")
+def hello():
+    return "<h1>Hey there!</h1>"
 
 app.run(debug=True) # http://127.0.0.1:5000/에서 브라우저 실행됨. 터미널에서 cmd+c하면 서버 꺼짐
 
